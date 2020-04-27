cce8bee39a5e5faa2d1564f612a615f69741cf70
Tram Anh Ngoc Tran <tiffany.tran1212@gmail.com>
2020-04-27 13:02:01 +0200

    json added - need to finish modal

---
+++ sections.js
@@ -0,0 +1,62 @@
+"use strict";
+
+window.addEventListener("load", init);
+
+const endpoint = "https://frontendspring20-67be.restdb.io/rest/dxc-technology";
+const apiKey = "5e95767d436377171a0c2333";
+
+function init() {
+  getData();
+  console.log("hi");
+}
+
+function getData() {
+  const data = fetch(endpoint, {
+    method: "get",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      "x-apikey": apiKey,
+      "cache-control": "no-cache",
+    },
+  })
+    .then((e) => e.json())
+    .then(handleData);
+  console.log("getting data");
+}
+
+function handleData(myData) {
+  myData.forEach(showBenefits);
+}
+
+function showBenefits(post) {
+  const template = document.querySelector("#benefitTemplate").content;
+  const copy = template.cloneNode(true);
+  const img = copy.querySelector(".benefit-img img");
+  copy.querySelector(".benefit").textContent = post.benefits;
+  img.setAttribute("src", post.benefits_img);
+  copy.querySelector(".benefit").addEventListener("click", () => {
+    fetch(endpoint + `?id=${copy.id}`, {
+      method: "get",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+        "x-apikey": apiKey,
+        "cache-control": "no-cache",
+      },
+    })
+      .then((res) => res.json())
+      .then(openModal);
+  });
+  document.querySelector(".section1-bottom-right").appendChild(copy);
+}
+
+function openModal(modal) {
+  document.querySelector("#modal-bg").classList.remove("hide");
+  document.querySelector("#modal-container").classList.remove("hide");
+  document.querySelector(".benefits-description").textContent =
+    modal.benefits_description;
+  console.log(modal);
+}
+function closeModal() {
+  document.querySelector("#modal-bg").classList.add("hide");
+  document.querySelector("#modal-container").classList.add("hide");
+}
