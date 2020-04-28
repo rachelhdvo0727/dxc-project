198dbf6e7b3cfb225bfcddfdc62ab39d40ac4c70
rachelvo <rach0727@stud.kea.dk>
2020-04-28 20:41:19 +0200

    done w redirect page

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 require("@babel/polyfill");
 import { gsap } from "gsap";
+import { signinForm } from "./signin-form";
+
+window.addEventListener("DOMContentLoaded", start);
+
+function start() {
+  console.log("start");
+  signinForm();
+}
--- signin-form.js
+++ signin-form.js
@@ -10,76 +10,82 @@ import {
 import { gsap } from "gsap";
 require("@babel/polyfill");
 
-window.addEventListener("DOMContentLoaded", start);
-
-function start() {
+export function signinForm() {
   form.setAttribute("novalidate", true);
   setupForm();
   document.querySelector("#submit").addEventListener("click", (evt) => {
     evt.preventDefault();
     checkInputsValue();
   });
-}
-function checkInputsValue() {
-  let validForm = true;
-  if (form.checkValidity() && validForm) {
-    postInfo({
-      first_name: elms.firstname.value,
-      last_name: elms.lastname.value,
-      email: elms.email.value,
-    });
-    form.reset();
-    showWelcome();
-  } else {
-    console.log("error messages");
+
+  function checkInputsValue() {
+    let validForm = true;
+    if (form.checkValidity() && validForm) {
+      postInfo({
+        first_name: elms.firstname.value,
+        last_name: elms.lastname.value,
+        email: elms.email.value,
+      });
+    } else {
+      console.log("error messages");
+      formElms.forEach((elm) => {
+        elm.classList.remove("invalid");
+        if (!elm.checkValidity()) {
+          elm.classList.add("invalid");
+        }
+      });
+      correctInputs();
+    }
+  }
+  function postInfo(data) {
+    const postData = JSON.stringify(data);
+    fetch(endpoint, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+        "x-apikey": apiKey,
+        "cache-control": "no-cache",
+      },
+      body: postData,
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        showWelcome();
+      });
+  }
+  function setupForm() {
+    console.log("no error messages");
     formElms.forEach((elm) => {
       elm.classList.remove("invalid");
-      if (!elm.checkValidity()) {
+      if (elm.checkValidity()) {
         elm.classList.add("invalid");
       }
     });
-    correctInputs();
   }
-}
-function postInfo(data) {
-  const postData = JSON.stringify(data);
-  fetch(endpoint, {
-    method: "post",
-    headers: {
-      "Content-Type": "application/json; charset=utf-8",
-      "x-apikey": apiKey,
-      "cache-control": "no-cache",
-    },
-    body: postData,
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-      showWelcome();
+  function correctInputs() {
+    console.log("correct inputs");
+    formElms.forEach((elm) => {
+      elm.addEventListener("keyup", () => {
+        elm.classList.remove("invalid");
+      });
     });
-}
-function setupForm() {
-  console.log("no error messages");
-  formElms.forEach((elm) => {
-    elm.classList.remove("invalid");
-    if (elm.checkValidity()) {
-      elm.classList.add("invalid");
-    }
-  });
-}
-function correctInputs() {
-  console.log("correct inputs");
-  formElms.forEach((elm) => {
-    elm.addEventListener("keyup", () => {
-      elm.classList.remove("invalid");
-    });
-  });
-}
-function showWelcome() {
-  console.log("welcome");
-  welcomeSec.classList.remove("hide");
-  signinSec.classList.add("hide");
-  document.querySelector(".back").classList.add("hide");
-  document.querySelector("#submit").classList.add("hide");
-  //goToAssetPage();
+  }
+  function showWelcome() {
+    console.log("welcome");
+    welcomeSec.classList.remove("hide");
+    signinSec.classList.add("hide");
+    document.querySelector(".back").classList.add("hide");
+    document.querySelector("#submit").classList.add("hide");
+    setTimeout(redirectToAssetPage, 2000);
+  }
+  function redirectToAssetPage() {
+    console.log("redirect");
+    //TODO: turn this on with correct html file 👇🏽
+    //location.href = "https://rhdvo.dk";
+    localStorage.setItem(
+      "user",
+      JSON.stringify(["Rachel", "Vo", "rach@gmail.com"])
+    );
+  }
 }
