733c927039eeeb98e9e76d2ee0bdefb421c3f77d
Tram Anh Ngoc Tran <tiffany.tran1212@gmail.com>
2020-04-28 12:21:55 +0200

    loader added - need to implement to js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 require("@babel/polyfill");
 import { gsap } from "gsap";
-import {intro} from "./"
+import { intro } from "./";
+
+$(window).on("load", function () {
+  $(".loader-wrapper").fadeOut("slow");
+});
