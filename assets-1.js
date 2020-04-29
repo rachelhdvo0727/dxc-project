3461052e8190f809e021c33b5b86302077a4661a
Subangi <suba0107@stud.kea.dk>
2020-04-29 22:12:22 +0200

    Fixed asset page

--- assets-1.js
+++ assets-1.js
@@ -1,21 +1,72 @@
 import { gsap } from "gsap";
 import { intro } from "./js_modules/intro";
-import { landing } from "./js_modules/landing";
+// import { landing } from "./js_modules/landing";
 require("@babel/polyfill");
 
 window.addEventListener("DOMContentLoaded", start);
 
 function start() {
   console.log("start");
-  landing();
+  // landing();
   intro();
   sections();
+  getSvg("svgs/sixfactors.svg", placeSVG);
 }
+
+function placeSVG(svg) {
+  console.log("placeSvg");
+  document.querySelector(".factors_img").innerHTML = svg;
+  document.querySelectorAll("#poly1-2, #poly2-2, #poly3-2, #poly4-2, #poly5-2, #poly6-2").forEach((elm) => {
+    elm.addEventListener("mouseover", mouseOverPoly);
+    elm.addEventListener("mouseout", mouseOutPoly);
+  });
+
+  document.querySelectorAll(".factors_img g").forEach((elm) => {
+    elm.addEventListener("mouseover", mouseOverG);
+    elm.addEventListener("mouseout", mouseOutG);
+  });
+}
+
+function mouseOverPoly(e) {
+  console.log(e.target);
+  e.target.style.fill = "#ffed00";
+  console.log(e.target.id);
+  document.querySelector(`#${e.target.id}-2`).style.stroke = "#000000";
+  document.querySelector(`#${e.target.id}-2`).style.fill = "#000000";
+}
+
+function mouseOutPoly(e) {
+  e.target.style.fill = "#000000";
+  document.querySelector(`#${e.target.id}-2`).style.stroke = "#ffffff";
+}
+
+function mouseOverG(e) {
+  console.log(e.target);
+  document.querySelectorAll(`#${e.target.id}`).forEach((elm) => {
+    elm.style.fill = "#000000";
+  });
+  document.querySelectorAll(`#${e.target.id}`).style.stroke = "#000000";
+  console.log(e.target.id);
+  const polyId = e.target.id.slice(0, e.target.id.length - 2);
+  console.log(polyId);
+  document.querySelector(`#${polyId}`).style.stroke = "#000000";
+  document.querySelector(`#${polyId}`).style.fill = "#000000";
+}
+
+function mouseOutG(e) {
+  e.target.style.fill = "#000000";
+  document.querySelector(`#${e.target.id}-2`).style.stroke = "#ffffff";
+}
+async function getSvg(filename, callback) {
+  let response = await fetch(filename);
+  let mySvgData = await response.text();
+  callback(mySvgData);
+}
+
 function sections() {
   startSections();
 
-  const endpoint =
-    "https://frontendspring20-67be.restdb.io/rest/dxc-technology?max=6";
+  const endpoint = "https://frontendspring20-67be.restdb.io/rest/dxc-technology?max=6";
   const apiKey = "5e95767d436377171a0c2333";
 
   function startSections() {
@@ -24,17 +75,14 @@ function sections() {
   }
 
   function getData() {
-    const data = fetch(
-      "https://frontendspring20-67be.restdb.io/rest/dxc-technology?max=6",
-      {
-        method: "get",
-        headers: {
-          "Content-Type": "application/json; charset=utf-8",
-          "x-apikey": "5e95767d436377171a0c2333",
-          "cache-control": "no-cache",
-        },
-      }
-    )
+    const data = fetch("https://frontendspring20-67be.restdb.io/rest/dxc-technology?max=6", {
+      method: "get",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+        "x-apikey": "5e95767d436377171a0c2333",
+        "cache-control": "no-cache",
+      },
+    })
       .then((e) => e.json())
       .then(handleData);
     console.log("getting data");
@@ -50,22 +98,16 @@ function sections() {
     const copy = template.cloneNode(true);
     const img = copy.querySelector(".benefit-img img");
     copy.querySelector(".benefit").textContent = post.benefits;
-    img.setAttribute(
-      "src",
-      `https://frontendspring20-67be.restdb.io/media/${post.benefits_img}`
-    );
+    img.setAttribute("src", `https://frontendspring20-67be.restdb.io/media/${post.benefits_img}`);
     copy.querySelector(".benefit").addEventListener("click", () => {
-      fetch(
-        `https://frontendspring20-67be.restdb.io/rest/dxc-technology/${post._id}`,
-        {
-          method: "get",
-          headers: {
-            "Content-Type": "application/json; charset=utf-8",
-            "x-apikey": "5e95767d436377171a0c2333",
-            "cache-control": "no-cache",
-          },
-        }
-      )
+      fetch(`https://frontendspring20-67be.restdb.io/rest/dxc-technology/${post._id}`, {
+        method: "get",
+        headers: {
+          "Content-Type": "application/json; charset=utf-8",
+          "x-apikey": "5e95767d436377171a0c2333",
+          "cache-control": "no-cache",
+        },
+      })
         .then((res) => res.json())
         .then(openModal);
       console.log(post);
@@ -76,8 +118,7 @@ function sections() {
   function openModal(modal) {
     document.querySelector("#modal-bg").classList.remove("hide");
     document.querySelector("#modal-container").classList.remove("hide");
-    document.querySelector(".benefits-description").textContent =
-      modal.benefits_description;
+    document.querySelector(".benefits-description").textContent = modal.benefits_description;
     document.querySelector(".close").addEventListener("click", closeModal);
     console.log(modal);
   }
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import { gsap } from "gsap";
-import { intro } from "./js_modules/intro";
+// import { intro } from "./js_modules/intro";
 import { landing } from "./js_modules/landing";
-import { sections } from "./js_modules/sections";
+// import { sections } from "./js_modules/sections";
 import { signupForm } from "./js_modules/form";
 import { signinForm } from "./js_modules/signin-form";
 require("@babel/polyfill");
